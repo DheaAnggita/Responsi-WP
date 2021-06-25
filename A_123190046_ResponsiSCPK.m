5d1f36e1cccda3f46c50ddcbcb5653c15c8373e9
Dhea Anggita <dhea.anggita18@gmail.com>
2021-06-25 21:57:03 +0700

    revisi

--- A_123190046_ResponsiSCPK.m
+++ A_123190046_ResponsiSCPK.m
@@ -1,10 +1,9 @@
 %penyelesaian kasus menggunakan metode WP 
 clc;clear; %untuk membersihkan jendela command windows 
 data = xlsread('Real Estate.xlsx', 'C2:E51');
-data1 = xlsread('Real Estate.xlsx', 'H2:H51');
+data1 = xlsread('Real Estate.xlsx', 'H2:H51'); %membaca file Real Estate.xlsx
 x = [data data1];
 k = [1,0,1,0]; %jenis kriteria 1 untuk keuntungan 0 untuk biaya
-%atribut tiap-tiap kriteria, dimana nilai 1=atrribut keuntungan, dan 0= atribut biaya
 w = [3, 5, 4, 1]; %Nilai bobot tiap kriteria 
 %tahapan pertama, perbaikan bobot
 [m n]=size (x); %inisialisasi ukuran x 
@@ -22,3 +21,20 @@ end;
 
 %tahapan ketiga, proses perangkingan 
 V= S/sum(S)
+
+B = sort(V, 'descend'); %sorting dari terbesar
+
+opts = detectImportOptions('Real Estate.xlsx');
+opts.SelectedVariableNames = (1);
+rekomendasi = readmatrix('Real Estate.xlsx', opts); %membaca file Real Estate.xlsx
+
+for i=1:5 %mengambil data peringkat 1 sampai 5
+    for j=1:m
+        if(B(i) == V(j))
+            alternatif(i) = "nomor " + rekomendasi(j);
+            break;
+        end
+    end
+end
+
+alternatif
